af18d1b63040aa94c3b63345c048534f587d7dd3
Nick Nauert <nicknauert@gmail.com>
2017-07-26 12:05:56 -0400

    final commit

--- main.js
+++ main.js
@@ -32,32 +32,44 @@ console.log(maxOfThree(4, 5, 21));
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // Then, write and example of using the function.
 
-function isVowel(char){
-    if (char === "a" || "e" || "i" || "o" || "u") {
+function isVowel(letter){
+    if (letter === "a" ||letter === "e" ||letter === "i" ||letter === "o" ||letter === "u") {
       return true;
-    } else {
-    return false;
+    }
+    else if  (letter !== "a" || "e" || "i" || "o" || "u") {
+      return false;
   }
 }
-console.log(isVowel("a"));
+console.log(isVowel("p"));
 
 
 // 4.
 // Write a function called `sum` that takes two parameters and returns the sum of those 2 numbers.
 // Then, write and example of using the function.
 
-
+  function sum(x, y) {
+    return x + y;
+  }
+console.log(sum(3, 5));
 
 // 5.
 // Write a function named `avg` that takes 3 parameters and returns the average of those 3 numbers.
 // Then, write and example of using the function.
 
+function avg(x, y, z) {
+  return (x + y + z)/3;
+}
 
+console.log(avg(3, 4, 10));
 
 // 6.
 // Write a function called `getLength` that takes one parameter (a string) and returns the length
 // Then, write and example of using the function.
 
+function getLength(string) {
+  return string.length;
+}
+console.log("There are " + getLength('barnacles') + " letters in that word");
 
 
 // 7.
@@ -66,6 +78,16 @@ console.log(isVowel("a"));
 // Otherwise the function should return `false`.
 // Then, write and example of using the function.
 
+function greaterThan (x, y) {
+  if (x>y) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log(greaterThan(5,12));
 
 
 // 8.
@@ -76,6 +98,12 @@ console.log(isVowel("a"));
 // Then, write and example of using the function.
 
 
+function greet(name){
+  return "Hello " + name + "!";
+}
+
+console.log(greet("Nick"));
+
 
 // 9.
 // Write a function called `madlib` that takes 4 or more parameters (words).
@@ -85,3 +113,10 @@ console.log(isVowel("a"));
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
 // Then, write and example of using the function.
+
+
+function madLib(adj, name, verb, noun) {
+  return adj + " " + name + " " + verb + "ed the " + noun + "."
+}
+
+console.log(madLib("Blue", "Bobby", "play", "the fiddle"));
